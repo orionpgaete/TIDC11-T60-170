4355697e458f652d12d13adf0eb28a1dc8d23c0e
orionpgaete <pedro.gaete@pgcomputer.cl>
2022-03-23 17:17:39 -0300

    admin persona

---
+++ AdminPersonas/AdminPersonas/Operaciones/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPersonas
+{
+    public partial class Program
+    {
+        static void MostrarPersonas()
+        {
+
+        }
+
+        static void BuscarPersonas()
+        {
+
+        }
+        static void IngresarPersona()
+        {
+            string nombre;
+            uint telefono;
+            double peso;
+            double estatura;
+
+            Console.WriteLine("Bienvenido al programa Ultra distintisimo y mas BKN");
+
+            bool esValido;
+
+            do
+            {
+                Console.WriteLine("Ingrese telefono");
+                /*string tel = Console.ReadLine().Trim();
+                esValido = UInt32.TryParse(tel, out telefono);*/
+
+                esValido = UInt32.TryParse(Console.ReadLine().Trim(), out telefono);
+            } while (!esValido);
+
+            do
+            {
+                Console.WriteLine("Ingrese peso");
+                esValido = double.TryParse(Console.ReadLine().Trim(), out peso);
+            } while (!esValido);
+
+            do
+            {
+                Console.WriteLine("Ingrese estatura");
+                esValido = double.TryParse(Console.ReadLine().Trim(), out estatura);
+            } while (!esValido);
+
+            do
+            {
+                Console.WriteLine("Ingrese nombre");
+                nombre = Console.ReadLine().Trim();
+            } while (nombre.Equals(string.Empty));
+
+            Persona p = new Persona();
+
+            Console.WriteLine("Nombre : {0}", nombre);
+            Console.WriteLine("Telefono : {0}", telefono);
+            Console.WriteLine("Peso : {0}", peso);
+            Console.WriteLine("Estatura : {0}", estatura);
+            Console.WriteLine("IMC : {0}", peso / (estatura * estatura));
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ AdminPersonas/AdminPersonas/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPersonas
+{
+    public partial class Program
+    {
+        static void Main (string [] a)
+        {
+            while( Menu());
+        }
+
+        static bool Menu()
+        {
+            bool continuar = true;
+            Console.WriteLine("1. Ingresar");
+            Console.WriteLine("2. Mostrar");
+            Console.WriteLine("3. Buscar");
+            Console.WriteLine("0. Salir");
+            switch (Console.ReadLine().Trim())
+            {
+                case "1": IngresarPersona();
+                    break;
+                case "2": MostrarPersonas();
+                    break;
+                case "3": BuscarPersonas();
+                    break;
+                case "0": continuar = false;
+                    break;
+                default: Console.WriteLine("Aprete el teclado bien gil¡¡¡¡¡");
+                    break;
+            }
+            return continuar;
+        }
+
+ 
+    }
+}
